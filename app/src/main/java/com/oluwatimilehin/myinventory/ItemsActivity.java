b4e86135d91836039dee2464f4aa10b385c4271d
Oluwatimilehin <timad97@Yahoo.com>
2017-01-25 20:27:09 +0100

    Change class name

--- app/src/main/java/com/oluwatimilehin/myinventory/ItemsActivity.java
+++ app/src/main/java/com/oluwatimilehin/myinventory/ItemsActivity.java
@@ -3,11 +3,11 @@ package com.oluwatimilehin.myinventory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_items);
     }
 }
