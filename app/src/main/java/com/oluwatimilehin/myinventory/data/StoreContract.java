a74deebdc90f02bb2049b8fe721b424bf0827168
Oluwatimilehin <timad97@Yahoo.com>
2017-01-25 19:32:45 +0100

    Define store contract

--- app/src/main/java/com/oluwatimilehin/myinventory/data/StoreContract.java
+++ app/src/main/java/com/oluwatimilehin/myinventory/data/StoreContract.java
@@ -1,5 +1,6 @@
 package com.oluwatimilehin.myinventory.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,8 +10,17 @@ import android.provider.BaseColumns;
 public class StoreContract {
 
     private StoreContract(){};
+    public static final String CONTENT_AUTHORITY = "com.oluwatimilehin.myinventory";
+    public static final String PATH_INVENTORY = "inventory";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY );
 
     public static class StoreEntry implements BaseColumns{
-
+        public static final Uri INVENTORY_URI = Uri.withAppendedPath(BASE_CONTENT_URI,
+                PATH_INVENTORY);
+        public static final String TABLE_NAME = "inventory";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_IMAGE = "image";
     }
 }
